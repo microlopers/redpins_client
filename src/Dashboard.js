f0004508e153b7a2b6fa0277464579f15ff7dfd5
Lubo Losonsky <lubo.losonsky@sk.ibm.com>
2020-05-13 15:18:48 +0200

    fixed gui

---
+++ src/Dashboard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Axios from 'axios';
+
+function Dashboard() {
+    let carDetails = null
+    function loadCardDetail(){
+        Axios.get('https://localhost:8443/cardetails', { crossdomain: true }).then(
+            res => {
+                carDetails = res.data;
+                console.log(carDetails);
+                console.log(carDetails.id);
+                console.log(carDetails.licencePlate);
+            }
+        )
+    }
+
+    return(
+        <div className="Dashboard">
+            <Box color="red" clone>
+                <Button onClick={loadCardDetail}>Car log</Button>
+                
+            </Box>
+        </div>
+    )
+}
+
+export default Dashboard
